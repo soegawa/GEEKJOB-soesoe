ca749058c1ff2e1fb12fb4685055f28fc24d1e3b
GEEKJOB-soesoe <threeokky@gmail.com>
2017-11-20 16:53:03 +0900

    Java メソッド4-3 課題提出します

---
+++ CampChallenge_java/011.メソッド/Servlet8.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.sample8;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Servlet8 extends HttpServlet {
+
+    String[] myprof(int num){
+        
+        
+        String[] data1 = {"1","添川陽太","平成3年2月19日","川崎"};
+        String[] data2 = {"2","鈴木一郎","平成4年3月20日",null};
+        String[] data3 = {"3","田中太郎","平成5年4月21日","新橋"};
+        
+        
+        if(num == 1){
+        return data1;
+        }else if(num == 2){
+        return data2;
+        }else if(num == 3){
+        return data3;
+        }
+        
+        return null;
+        
+        
+        
+     }  
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        Integer limit = 2;
+        for(int n=1;n<=limit;n++){
+        for(int i=1;i<4;i++){ 
+        if(myprof(n)[i]==null){
+        continue;
+        }
+        out.print(myprof(n)[i]+"<br>");
+        
+          }
+         }
+       } 
+          
+            
+      }
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
