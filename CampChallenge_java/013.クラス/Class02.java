f68227d392abd379290d2c4e3e499479a1809f29
GEEKJOB-soesoe <threeokky@gmail.com>
2017-11-21 15:09:18 +0900

    Java クラス2-1 課題提出します

---
+++ CampChallenge_java/013.クラス/Class02.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.sample1;
+
+  class Clear extends Human {
+
+    public void setClear(){
+       this.name = null;
+        this.age = 0;
+    }
+    
+}
+
+
+public class Class02 {
+    
+    
+}
