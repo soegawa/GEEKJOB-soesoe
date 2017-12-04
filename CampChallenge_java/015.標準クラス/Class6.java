1d3017f5c6a5b2315c2af09312bca9deb576d0e1
GEEKJOB-soesoe <threeokky@gmail.com>
2017-12-04 11:09:57 +0900

    Java 標準クラス2-2 課題提出します

---
+++ CampChallenge_java/015.標準クラス/Class6.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.sample1;
+
+
+public class Class6 {
+    public static void main(String[] args){
+      
+        String myadd = "tweokky@gmail.com";
+
+        System.out.print(myadd.substring(myadd.indexOf("@")));
+  }
+}
