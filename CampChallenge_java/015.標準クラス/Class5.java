58482c15c4a46e64c5b04bf3a4fd6e9983849ca7
GEEKJOB-soesoe <threeokky@gmail.com>
2017-12-04 10:58:51 +0900

    Java 標準クラス 2-1 課題提出します

---
+++ CampChallenge_java/015.標準クラス/Class5.java
@@ -0,0 +1,12 @@
+package org.camp.sample1;
+
+
+public class Class5 {
+    
+    public static void main(String[] args){
+        String name = "添川陽太";
+        System.out.println(name.getBytes().length);
+        System.out.println(name.length());
+        
+    }
+}    
